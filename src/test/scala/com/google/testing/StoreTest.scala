ab80fcfe6693218a8eb4103eb3d6b8b7a059340a
zhihan <zhi.han@gmail.com>
2016-01-31 08:24:52 -0500

    Added test for result

--- src/test/scala/com/google/testing/StoreTest.scala
+++ src/test/scala/com/google/testing/StoreTest.scala
@@ -25,4 +25,17 @@ class StoreTest extends FunSuite with BeforeAndAfter {
     val x = Store.nextReady
     assert(x == Some(ts))
   }
+
+  test("No result") {
+    val r = Store.getResult(0)
+    assert(r == None)
+  }
+
+  test("Has result") {
+    val t = Store.testStates(10)
+    Store.updateTest(t.copy(result = Some("result")))
+
+    val r = Store.getResult(10)
+    assert(r == Some("result"))
+  }
 }
